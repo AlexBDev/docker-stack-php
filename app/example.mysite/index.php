63cab7a17cdb9f4aa35be962f8049ec7a7e6a9e8
Alexis Bussi?res <bussieres.alexis.dev@gmail.com>
2017-06-13 16:10:51 +0200

    Remove run.sh start nginx from script throw error, add working example

---
+++ app/example.mysite/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '<h1 style="text-align: center">@AlexBDev</h1>';
+echo '<h2 style="text-align: center">Enjoy the stack</h2>';
+echo phpinfo();
--- docker/dockerfiles/php/71/script/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start services here
-sudo /usr/sbin/nginx
-
-# These line need to be the last to maintain the container up
-tail -f /dev/null
